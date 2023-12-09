c07b62773e7a13487732eb766969e1c69fb53c8b
PHunt14 <PHunt14@users.noreply.github.com>
2023-12-08 22:04:28 -0500

    chapter4: stopping partway through module inputs

---
+++ chapter4/prod/services/webserver-cluster/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+terraform {
+    backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform-up-and-running-state-ph"
+    key            = "chapter4/prod/services/webserver-cluster/terraform.tfstate"
+    region         = "us-east-2"
+
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
+
+module "webserver_cluster" {
+  source = "../../../modules/services/webserver-cluster"
+
+  cluster_name           = "webservers-prod"
+  db_remote_state_bucket = "terraform-up-and-running-state-ph"
+  db_remote_state_key    = "prod/data-stores/mysql/terraform.tfstate"
+}
